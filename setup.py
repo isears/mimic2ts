6c5853753ce99dd49d6a4bf4bb2d0f305cf437f1
Isaac Sears <isaac.j.sears@gmail.com>
2022-06-07 13:03:55 -0400

    Basic python packaging boilerplate

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mimic2ts",
+    version="0.0.1",
+    author="Isaac Sears",
+    author_email="isaac.j.sears@gmail.com",
+    description="Convert MIMIC IV tabular data into timeseries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/isears/mimic2ts",
+    project_urls={
+        "Bug Tracker": "https://github.com/isears/mimic2ts/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
