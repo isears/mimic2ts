f4625ea89c818d780d4a65b27d761d17e676de3e
isears <isaac.j.sears@gmail.com>
2022-06-23 18:06:23 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mimic2ts",
-    version="0.1.0",
+    version="0.1.1",
     author="Isaac Sears",
     author_email="isaac.j.sears@gmail.com",
     description="Convert MIMIC IV tabular data into timeseries",
